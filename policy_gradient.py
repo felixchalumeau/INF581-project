6b5423e75a4a5ff90e53262ba26bc5effd8ce88a
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-03-03 01:36:35 +0100

    first acceptable results

--- policy_gradient.py
+++ policy_gradient.py
@@ -5,12 +5,16 @@ import random
 
 # TODO: explain following
 ENV_NAME = "CarRacing-v0"
-EPISODE_DURATION = 300
+EPISODE_DURATION = 15000
+EPISODE_DURATION_AUGM = 25
 ALPHA_INIT = 0.1 # (can also decay this over time..)
-SCORE = 200
-TEST_TIME = 100
-LEFT = 0
-RIGHT = 1
+SCORE = 900
+TEST_TIME = 1
+TEST_TIME_AUGM = 1
+LEFT = np.array([-1, 0, 0])
+RIGHT = np.array([1, 0, 0])
+
+EPSILON = 0.05
 
 VERBOSE = True
 
@@ -45,32 +49,33 @@ def capteur(observation):
     j_right = 49
     
     grass_color = 180
+    road_color = 102
 
     # informations horizontales
     horizontal = np.where(observation[67] == grass_color)[0]
     try:
         hori_gauche = 46 - horizontal[horizontal < 46][-1]
     except:
-        hori_gauche = -1
+        hori_gauche = 46
     try:
         hori_droite = horizontal[horizontal > 49][0] - 49
     except:
-        hori_droite = -1
+        hori_droite = 46
     
     # informations verticales
     vertical_gauche = np.where(observation[:, 46] == grass_color)[0]
     try:
-        verti_gauche = 67 - vertical_gauche[vertical_gauche < 67][-1]
+        verti_gauche = 67 - vertical_gauche[vertical_gauche < 67][-1] 
     except:
-        verti_gauche = -1
+        verti_gauche = 67
 
     vertical_droite = np.where(observation[:, 49] == grass_color)[0]
     try:
         verti_droite = 67 - vertical_droite[vertical_droite < 67][-1]
     except:
-        verti_droite = -1
+        verti_droite = 67
 
-    return [hori_gauche, hori_droite, verti_gauche, verti_droite]
+    return np.array([hori_gauche, hori_droite, verti_gauche, verti_droite])/20
 
 # a kind of complete preprocessing of the images observed
 def useful_from_observation(rgb):
@@ -90,28 +95,35 @@ def sigmoid(x):
 def get_policy(s, theta):
 
     p_right = sigmoid(np.dot(s, np.transpose(theta)))
-    pi = [1-p_right, p_right]
-    return pi
+    # pi = [1-p_right, p_right]
+    # return pi
+    return 2*p_right - 1
 
 # Draw an action according to current policy
 def act_with_policy(s, theta):
-    p_right = get_policy(s, theta)[1]
+    # p_right = get_policy(s, theta)[1]
+    p_right = get_policy(s, theta)
     r = np.random.rand()
-    if r < p_right:
-        return np.array([1, 0, 0])
-    else:
-        return np.array([-1, 0, 0])
+    # if r < EPSILON:
+    #    return np.array([1, 0, 0])
+    #else:
+    #    return np.array([-1, 0, 0])
+    turn = np.clip(np.array([p_right + r*EPSILON]), a_min = -1, a_max = 1)[0][0]
+    return np.array([turn, 0.0, 0.0])
 
 # Generate an episode
 def gen_rollout(env, theta, max_episode_length=EPISODE_DURATION, render=False):
 
     s_t = env.reset()
     s_t = useful_from_observation(s_t)
+    s_t = np.concatenate((s_t, np.array([0, 0, 0, 0])))
 
     episode_states = []
     episode_actions = []
     episode_rewards = []
     episode_states.append(s_t)
+
+    to_stop = 15
 
     for t in range(max_episode_length):
 
@@ -120,19 +132,30 @@ def gen_rollout(env, theta, max_episode_length=EPISODE_DURATION, render=False):
 
         a_t = act_with_policy(s_t, theta)
 
-        if t%2 == 0:
-            a_t += np.array([0, 1, 0])
+        if t%3 == 0:
+            a_t += np.array([0.0, 1.0, 0.0])
 
         s_t, r_t, done, info = env.step(a_t)
-        print(r_t)
         
+        # transform the state to a usable one
         s_t = useful_from_observation(s_t)
+        old_s_t = episode_states[-1][:4]
+        s_t = np.concatenate((s_t, s_t-old_s_t))
+        
+        # working on the reward
+        # r_t += (- s_t[0]**2 - s_t[1]**2)
+        if ((s_t[:4]==np.array([0.05, 0.05, 0.05, 0.05])).all()):
+            r_t = r_t - 1
+            to_stop = to_stop - 1
+        elif (s_t[0]==0.05 or s_t[1]==0.05):
+            r_t = r_t - 0.5
 
         episode_states.append(s_t)
         episode_actions.append(a_t)
         episode_rewards.append(r_t)
 
-        if done:
+
+        if done or to_stop==0:
             break
 
     return episode_states, episode_actions, episode_rewards
@@ -174,11 +197,12 @@ def compute_PG(episode_states, episode_actions, episode_rewards, theta):
         a_t = episode_actions[t]
         R_t = sum(episode_rewards[t::])
 
-        if a_t == LEFT:
-            g_theta_log_pi = - pi[RIGHT] * episode_states[t] * R_t
-        else:
-            g_theta_log_pi = pi[LEFT] * episode_states[t] * R_t
+        #if (a_t[0] == 1):
+        #    g_theta_log_pi = - pi[1] * episode_states[t] * R_t
+        #else:
+        #    g_theta_log_pi = pi[0] * episode_states[t] * R_t
         
+        g_theta_log_pi = pi * episode_states[t] * R_t
         PG += g_theta_log_pi
 
     return PG
@@ -190,14 +214,17 @@ def train(env, theta_init, max_episode_length = EPISODE_DURATION, alpha_init = A
     i_episode = 0
     average_returns = []
 
-    success, _, R = test_policy(env, theta)
+    success, _, R = test_policy(env, theta, render=True)
     average_returns.append(R)
+
+    new_max_episode_length = max_episode_length
+    new_test_time = TEST_TIME
 
     # Train until success
     while (not success):
 
         # Rollout
-        episode_states, episode_actions, episode_rewards = gen_rollout(env, theta, max_episode_length)
+        episode_states, episode_actions, episode_rewards = gen_rollout(env, theta, max_episode_length, render=True)
 
         # Schedule step size
         alpha = alpha_init / (1 + i_episode)
@@ -209,7 +236,8 @@ def train(env, theta_init, max_episode_length = EPISODE_DURATION, alpha_init = A
         theta += alpha * PG
 
         # Test new policy 
-        success, _, R = test_policy(env, theta, render=False)
+        # new_test_time += TEST_TIME_AUGM
+        success, _, R = test_policy(env, theta, score=SCORE, num_episodes=new_test_time, max_episode_length= max_episode_length, render=True)
 
         # Monitor 
         average_returns.append(R)
@@ -229,7 +257,7 @@ def main():
     # dim = env.observation_space.shape[0]
 
     # Init parameters to random
-    theta_init = np.random.randn(1, 4)
+    theta_init = np.random.randn(1, 8)
 
     # Train agent
     theta, i, average_returns = train(env, theta_init)
