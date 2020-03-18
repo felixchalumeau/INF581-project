c0b105ee9f9bac2a90d15dc57d65b9aef01bf1b1
pierrefernandezgenestier <pierre.fernandezgenestier@users.noreply.github.com>
2020-03-18 14:51:38 +0100

    added play file and first dqn agent

--- test_envs.py
+++ test_envs.py
@@ -1,25 +1,20 @@
 import gym
 import numpy as np
 import random
+import tensorflow.keras as keras
+import seaborn as sns
 
 env = gym.make('CarRacing-v0')
-env.seed(42)
 
 new_action_space = [np.array([-1, 0, 0]), np.array([1, 0, 0]), np.array([0, 1, 0]), np.array([0, 0, 0]), np.array([0, 0, 1])]
 
 for i_episode in range(2):
     observation = env.reset()
     for t in range(200):
-        env.render()
-        print(observation)
-        
+        env.render()        
         action = random.choice(new_action_space)
         observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
 env.close()
-print("State space dimension is:", env.observation_space.shape[0])
-print("State upper bounds:", env.observation_space.high)
-print("State lower bounds:", env.observation_space.low)
-# print("Number of actions is:", env.action_space.n)
